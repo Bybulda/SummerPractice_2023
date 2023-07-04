895fbb4b0176d687ed82191a2e69048e136ae3d9
Nikita <kakeino@yandex.ru>
2023-07-04 19:50:12 +0300

    student half done

---
+++ Laby_fund/task1/Student.cs
@@ -0,0 +1,70 @@
+﻿namespace Laby_fund.task1;
+
+public enum Practice: int{
+    Cs,
+    Yandex,
+    Go,
+    DataSet,
+    Structures
+}
+
+public class Student
+{
+    public Student(string? name, string? surname, string? patronymic, string? group, Practice? choice)
+    {
+        NameField = name ?? throw new ArgumentNullException(nameof(name));
+        SurnameField = surname ?? throw new ArgumentNullException(nameof(surname));
+        PatronymicField = patronymic ?? throw new ArgumentNullException(nameof(patronymic));
+        GroupField = group ?? throw new ArgumentNullException(nameof(group));
+        PracticeField = choice ?? throw new ArgumentNullException(nameof(choice));
+    }
+
+    public string NameField
+    {
+        get;
+        private set;
+    }
+
+    public string SurnameField
+    {
+        get;
+        private set;
+    }
+
+    public string PatronymicField
+    {
+        get;
+        private set;
+    }
+
+    public string GroupField
+    {
+        get;
+        private set;
+    }
+    
+    private Practice PracticeField
+    {
+        get;
+        set;
+    }
+
+    public string PracticeType
+    {
+        get
+        {
+            string[] type = { "C#", "Yandex", "Go", "Dataset", "Structures" };
+            return type[(int) PracticeField];
+        }
+    }
+
+    public int CourseNumber
+    {
+        get
+        {
+            var number = GroupField.Split("-");
+            return Convert.ToInt32(number[1].Substring(0, 1));
+
+        }
+    }
+}
---
+++ Laby_fund/task1/test.cs
@@ -0,0 +1,22 @@
+﻿namespace Laby_fund.task1;
+
+class Test
+{
+    static int Main()
+    {
+        try
+        {
+            var stud = new Student("Nikita", "Smirnov", 
+                "Evhgenoevich", "M8O-211B-21", Practice.Cs);
+            Console.WriteLine($"My name is {stud.NameField}\nMy surname is {stud.SurnameField}\nMy patronymic is {stud.PatronymicField}\n" +
+                              $"I am from group {stud.GroupField}\nThe course is {stud.CourseNumber}\nI practice on {stud.PracticeType}");
+        }
+        catch (ArgumentNullException e)
+        {
+            Console.WriteLine(e.Message);
+            return -1;
+        }
+        
+        return 0;
+    }
+}
